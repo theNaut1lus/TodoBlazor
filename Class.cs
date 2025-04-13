a52ec8091c952051ea2eecd0ddce7bc67d9619cf
Sidney Aulakh <sidneyaulakh@gmail.com>
2025-04-13 22:22:38 +1000

    todo component and route, rendering and todoitem class

---
+++ Class.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoBlazor
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool isDone { get; set; } = false;
+    }
+}
---
+++ TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoBlazor
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
